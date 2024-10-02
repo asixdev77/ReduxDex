df4a0a7845e1bb6bc76076ae8dce439882b32b4f
Eugene <eugene.pa77@gmail.com>
2024-10-03 04:05:49 +0900

    fixed : react, react-dom, react-redux version

--- src/components/AlertModal/index.tsx
+++ src/components/AlertModal/index.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import {Modal as BsModal, Button} from 'react-bootstrap';
-import './index.scss';
+import React, { ReactNode } from 'react';
+import { Modal as BsModal, Button } from 'react-bootstrap';
 
-const AlertModal = (props) => {
-    return (
-        <BsModal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered>
-            {/* <BsModal.Title id="contained-modal-title-vcenter">
-                Note
-            </BsModal.Title> */}
-            <BsModal.Body>
-                <p>{props.alertmodaltext}</p>
-            </BsModal.Body>
-            <BsModal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </BsModal.Footer>
-        </BsModal>
-    );
+interface AlertModalProps {
+  alertmodaltext: ReactNode;
+  onHide: () => void;
+  show: boolean;
+  size?: 'sm' | 'lg' | 'xl';
+}
+
+const AlertModal: React.FC<AlertModalProps> = (props) => {
+  return (
+    <BsModal
+      {...props}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <React.Fragment>
+        <BsModal.Body>
+          <p>{props.alertmodaltext}</p>
+        </BsModal.Body>
+        <BsModal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </BsModal.Footer>
+      </React.Fragment>
+    </BsModal>
+  );
 };
 
 export default AlertModal;
